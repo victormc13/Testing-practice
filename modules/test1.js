73322b1bc835bda30cb42ffe7c4bcb800c0b9e76
victormc13 <viktorsb14@gmail.com>
2023-05-23 22:16:58 -0500

    Add Jest installation

--- modules/test1.js
+++ modules/test1.js
@@ -1,5 +1,18 @@
 function stringLength(string) {
   const length = string.length;
 
+  if (length === 0) {
+    throw new Error('String must contain at least 1 character.');
+  }
+
+  if (length > 10) {
+    throw new Error('String cannot exceed 10 characters.');
+  }
+
   return length;
 }
+
+module.exports = stringLength;
+
+
+module.exports = stringLength;
